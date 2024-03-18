e0b8ea433d59942e73f97f9ba48c35a950be367b
admiraladmirable <rick.morrow1204@gmail.com>
2024-03-18 12:10:52 -0500

    k3s setup

--- modules/ops/k8.nix
+++ modules/ops/k8.nix
@@ -5,13 +5,14 @@ with lib;
   options.k8.enable = mkEnableOption "Enable k8";
 
   config = mkIf config.k8.enable {
+    services.k3s.enable = true;
+    services.k3s.role = "server";
     users.users.rick-topl = {
       packages = with pkgs; [
         kubernetes-helm
         kubectl
-        kind
+        k3s
         terraform
-        awscli2
       ];
     };
   };
