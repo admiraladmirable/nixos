e297de010b53f35fee93b9ed58edc1ec99ad047f
admiraladmirable <rick.morrow1204@gmail.com>
2024-03-19 09:27:10 -0500

    undo

--- modules/display/xrdp.nix
+++ modules/display/xrdp.nix
@@ -7,6 +7,5 @@ with lib;
     services.xrdp.enable = true;
     services.xrdp.defaultWindowManager = "startplasma-x11";
     services.xrdp.openFirewall = true;
-    services.xrdp.audio.enable = true;
   };
 }
