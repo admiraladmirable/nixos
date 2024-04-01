1c1c79def51254ab7640894279edd20dc86a4a6a
admiraladmirable <rick.morrow1204@gmail.com>
2024-04-01 13:31:13 -0500

    update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/staging-next"; # temp fix for CVE
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
