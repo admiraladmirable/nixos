6fc8b4c3e9d0013040a858e3a403839d90af78bc
admiraladmirable <rick.morrow1204@gmail.com>
2024-05-22 10:59:35 -0500

    nvidia beta driver

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -79,15 +79,13 @@
     powerManagement.finegrained = false;
     nvidiaSettings = true;
     # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = {
-      config.boot.kernelPackages.nvidiaPackages.mkDriver = {
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
         version = "555.42.02";
         sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
         sha256_aarch64 = lib.fakeSha256;
         openSha256 = lib.fakeSha256;
-        settingsSha256 = lib.fakeSha256; 
-        persistencedSha256 = lib.fakeSha256;
-      };
+        settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA="; 
+        persistencedSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
     };
   };
 
